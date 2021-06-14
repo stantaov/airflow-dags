e1955c0ec4d5d27294aeb7ead2771cbdd08b5f95
Stan Taov <51180822+stantaov@users.noreply.github.com>
2021-06-13 22:13:15 -0400

    Update spark-job.py

--- spark-job.py
+++ spark-job.py
@@ -105,56 +105,56 @@ def emr_clent(region_name):
     global emr
     emr = boto3.client('emr', region_name=region_name)
 
-# # Creates an EMR cluster
-# def create_cluster(region_name, cluster_name='Spark-Cluster'):
-#     cluster = emr.run_job_flow(
-#         Name=cluster_name,
-#         ReleaseLabel='emr-5.33.0',
-#         Instances={
-#             'InstanceGroups': [
-#                 {
-#                     'Name': "Master Node",
-#                     'Market': 'ON_DEMAND',
-#                     'InstanceRole': 'MASTER',
-#                     'InstanceType': 'm3.xlarge',
-#                     'InstanceCount': 1
-#                 },
-#                 {
-#                     'Name': "Worker Node",
-#                     'Market': 'ON_DEMAND',
-#                     'InstanceRole': 'CORE',
-#                     'InstanceType': 'm3.xlarge',
-#                     'InstanceCount': 3
-#                 }
-#             ],
-#             'KeepJobFlowAliveWhenNoSteps': True,
-#             'Ec2KeyName' : 'midterm',
-#             'EmrManagedMasterSecurityGroup': 'sg-0916a76e1f13d70c9',
-#             'EmrManagedSlaveSecurityGroup': 'sg-095446c5e365749e1',
-#         },
-# 		# adding HIVE and Spark metastores to regester them later with Glue Data Catalog
-# 		Configurations=[{
-# 			'Classification': 'hive-site',
-# 			'Properties': {
-# 				'hive.metastore.client.factory.class': 'com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory'
-# 			}
-# 		}, 
-# 			{
-# 			'Classification': 'spark-hive-site',
-# 			'Properties': {
-# 				'hive.metastore.client.factory.class': 'com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory'
-# 			}
-# 		}],
-#         VisibleToAllUsers=True,
-#         JobFlowRole='EMR_EC2_DefaultRole',
-#         ServiceRole='EMR_DefaultRole',
-#         Applications=[
-#             { 'Name': 'Hadoop'},
-#             { 'Name': 'Hive'},
-#             { 'Name': 'Spark'}
-#         ]
-#     )
-#     return cluster['JobFlowId']
+# Creates an EMR cluster
+def create_cluster(region_name, cluster_name='Spark-Cluster'):
+    cluster = emr.run_job_flow(
+        Name=cluster_name,
+        ReleaseLabel='emr-5.33.0',
+        Instances={
+            'InstanceGroups': [
+                {
+                    'Name': "Master Node",
+                    'Market': 'ON_DEMAND',
+                    'InstanceRole': 'MASTER',
+                    'InstanceType': 'm3.xlarge',
+                    'InstanceCount': 1
+                },
+                {
+                    'Name': "Worker Node",
+                    'Market': 'ON_DEMAND',
+                    'InstanceRole': 'CORE',
+                    'InstanceType': 'm3.xlarge',
+                    'InstanceCount': 3
+                }
+            ],
+            'KeepJobFlowAliveWhenNoSteps': True,
+            'Ec2KeyName' : 'midterm',
+            'EmrManagedMasterSecurityGroup': 'sg-0916a76e1f13d70c9',
+            'EmrManagedSlaveSecurityGroup': 'sg-095446c5e365749e1',
+        },
+		# adding HIVE and Spark metastores to regester them later with Glue Data Catalog
+		Configurations=[{
+			'Classification': 'hive-site',
+			'Properties': {
+				'hive.metastore.client.factory.class': 'com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory'
+			}
+		}, 
+			{
+			'Classification': 'spark-hive-site',
+			'Properties': {
+				'hive.metastore.client.factory.class': 'com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory'
+			}
+		}],
+        VisibleToAllUsers=True,
+        JobFlowRole='EMR_EC2_DefaultRole',
+        ServiceRole='EMR_DefaultRole',
+        Applications=[
+            { 'Name': 'Hadoop'},
+            { 'Name': 'Hive'},
+            { 'Name': 'Spark'}
+        ]
+    )
+    return cluster['JobFlowId']
 
 # # Gets the EMR current cluster status
 # def get_cluster_status(cluster_id):
